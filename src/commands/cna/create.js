c3bbbc144f4d0243bb4241c2e4c0b21884cd2b3f
Jesse MacFadyen <purplecabbage@gmail.com>
2019-05-08 16:18:54 -0700

    linting, license header, missing eslint modules

--- src/commands/cna/create.js
+++ src/commands/cna/create.js
@@ -1,12 +1,23 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 
-const {Command, flags} = require('@oclif/command')
-const {cli} = require('cli-ux')
+const { Command, flags } = require('@oclif/command')
+const { cli } = require('cli-ux')
 const path = require('path')
 const fs = require('fs-extra')
 const tmp = require('tmp')
 const spawn = require('cross-spawn')
 const which = require('which')
-const npa = require('npm-package-arg')
+
+// const npa = require('npm-package-arg')
 
 // 'https://registry.npmjs.org/' // <= this is the normal default
 const DEFAULT_REGISTRY = 'https://artifactory.corp.adobe.com/artifactory/api/npm/npm-adobe-release/'
@@ -16,24 +27,24 @@ const DEFAULT_REGISTRY = 'https://artifactory.corp.adobe.com/artifactory/api/npm
 function getSelfDestructingTempDir () {
   return tmp.dirSync({
     prefix: 'cna-create-',
-    unsafeCleanup: false,
+    unsafeCleanup: false
   }).name
 }
 
-function isNpmInstalled() {
-  return which.sync('npm', {nothrow: true}) !== null
+function isNpmInstalled () {
+  return which.sync('npm', { nothrow: true }) !== null
 }
 
 class CNACreate extends Command {
-  async run() {
-    const {args, flags} = this.parse(CNACreate)
+  async run () {
+    const { args, flags } = this.parse(CNACreate)
 
     // 1. make path absolute
     let destDir = path.resolve(args.path)
 
     // 2. Make sure we have npm, fatal otherwise
     if (!isNpmInstalled()) {
-      this.error("oops, npm is required.")
+      this.error('oops, npm is required.')
     }
 
     // 3. create destination if not there
@@ -51,30 +62,30 @@ class CNACreate extends Command {
     let tmpDest = getSelfDestructingTempDir()
 
     // 5b call npm i with spec+temp temp destination
-    fs.ensureDirSync(path.join(tmpDest,'node_modules'))
+    fs.ensureDirSync(path.join(tmpDest, 'node_modules'))
     let res = this.npmInstall(flags.template, tmpDest, flags.registry || DEFAULT_REGISTRY)
-    res.then( () => {
+    res.then(() => {
       // console.log('success ... ' + tmpDest)
-      // 5c copy files from temp to dest 
+      // 5c copy files from temp to dest
 
-      let srcDir = path.join(tmpDest,'node_modules',flags.template)
+      let srcDir = path.join(tmpDest, 'node_modules', flags.template)
       fs.copySync(srcDir, destDir)
 
       cli.action.start('installing dependencies')
-      const child = spawn('npm', ['install'], { 
-        cwd: destDir, 
+      const child = spawn('npm', ['install'], {
+        cwd: destDir,
         stdio: 'inherit',
         env: process.env
       })
       child.on('error', err => {
-        cli.action.stop('failed') 
-        console.log('error ' + error)
+        cli.action.stop('failed')
+        console.log('error ' + err)
       })
       child.on('close', (code, sig) => {
         if (code !== 0) {
-          cli.action.stop('failed') 
+          cli.action.stop('failed')
         } else {
-          cli.action.stop('good') 
+          cli.action.stop('good')
         }
       })
     }).catch(err => {
@@ -82,31 +93,31 @@ class CNACreate extends Command {
     })
   }
 
-  async npmInstall(spec, dest, registry) {
+  async npmInstall (spec, dest, registry) {
     let cmd = 'npm'
 
     let result = new Promise((resolve, reject) => {
-      let env = Object.assign({},process.env)
-      env = Object.assign(env,{npm_config_registry:registry}) 
-      
+      let env = Object.assign({}, process.env)
+      env = Object.assign(env, { npm_config_registry: registry })
+
       cli.action.start('starting a process')
 
-      const child = spawn(cmd, ['install', spec], { 
-        cwd:dest, 
-        stdio: 'inherit', 
-        env:env
+      const child = spawn(cmd, ['install', spec], {
+        cwd: dest,
+        stdio: 'inherit',
+        env: env
       })
       child.on('error', err => {
-        cli.action.stop('failed') 
-        reject({ command: `${cmd} ${spec}`})
+        cli.action.stop('failed')
+        reject(err)
       })
       child.on('close', (code, sig) => {
         // console.log('close : ', code, sig)
         if (code !== 0) {
-          cli.action.stop('failed') 
-          reject({ command: `${cmd} install ${spec}`})
+          cli.action.stop('failed')
+          reject(new Error(`Failed to install ${spec}`))
         } else {
-          cli.action.stop() 
+          cli.action.stop()
           resolve()
         }
       })
@@ -116,7 +127,7 @@ class CNACreate extends Command {
 
   // todo: use this to do the copy so we can ignore some stuff, especially relevant when we support creating
   // from templates on the local filesystem
-  async copyTemplateFiles(templateDir, projectDir) {
+  async copyTemplateFiles (templateDir, projectDir) {
     const dirList = fs.readdirSync(templateDir)
     // skip directories, and files that are unwanted
     let excludes = ['.git', 'NOTICE', 'LICENSE', 'COPYRIGHT', '.npmignore', '.gitignore', 'node_modules']
@@ -137,10 +148,10 @@ CNACreate.args = [
   {
     name: 'path',
     description: 'Directory to create the app in',
-    default: '.',
-  },
+    default: '.'
+  }
   // todo: support a specified name that is used to populate package.json ...
-  // { 
+  // {
   //   name: 'name',
   //   default: 'MyApp',
   // },
@@ -150,11 +161,11 @@ CNACreate.flags = {
   template: flags.string({
     char: 't',
     description: 'Template starter filepath, git-url or published id/name.',
-    default: '@io-dev-tools/runtime-cna-starter'
+    default: '@adobe/io-cna-starter-project'
   }),
   registry: flags.string({
-    char:'r',
-    description:'Alternate registry to use. Passed into npm as environmental variable `npm_config_registry`'
+    char: 'r',
+    description: 'Alternate registry to use. Passed into npm as environmental variable `npm_config_registry`'
   }),
   verbose: flags.boolean({ char: 'd', description: 'Show verbose/debug output' }),
   help: flags.boolean({ char: 'h', description: 'Show help' })
