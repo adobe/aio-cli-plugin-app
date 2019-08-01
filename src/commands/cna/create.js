81ae30705ffd38be0d2d403a101e6b2ee2696696
Jesse MacFadyen <purplecabbage@gmail.com>
2019-08-01 15:58:24 -0700

    cleanup unused template files (#32)
    * cleanup unused template files
    
    * linting errors

--- src/commands/cna/create.js
+++ src/commands/cna/create.js
@@ -9,106 +9,44 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const { flags } = require('@oclif/command')
 const CNABaseCommand = require('../../CNABaseCommand')
-const { cli } = require('cli-ux')
-const path = require('path')
-const fs = require('fs-extra')
-const spawn = require('cross-spawn')
-const which = require('which')
+const InitCommand = require('./init')
 
-const templateMap = require('../../templates')
-
-function isNpmInstalled () {
-  return which.sync('npm', { nothrow: true }) !== null
-}
-
-function installTemplate (destDir) {
-  // todo: apply name from flags to package.json
-  cli.action.start('installing dependencies')
-  const child = spawn('npm', ['install'], {
-    cwd: destDir,
-    stdio: 'inherit',
-    env: process.env
-  })
-  child.on('error', err => {
-    cli.action.stop('failed')
-    console.log('error ' + err)
-  })
-  child.on('close', (code, sig) => {
-    if (code !== 0) {
-      cli.action.stop('failed')
-    } else {
-      cli.action.stop('good')
-    }
-  })
-}
+// const { cli } = require('cli-ux')
+// const spawn = require('cross-spawn')
+// function npmInstall (destDir) {
+//   // todo: apply name from flags to package.json
+//   cli.action.start('installing dependencies')
+//   const child = spawn('npm', ['install'], {
+//     cwd: destDir,
+//     stdio: 'inherit',
+//     env: process.env
+//   })
+//   child.on('error', err => {
+//     cli.action.stop('failed')
+//     console.log('error ' + err)
+//   })
+//   child.on('close', (code, sig) => {
+//     if (code !== 0) {
+//       cli.action.stop('failed')
+//     } else {
+//       cli.action.stop('good')
+//     }
+//   })
+// }
 
 class CNACreate extends CNABaseCommand {
   async run () {
-    const { args, flags } = this.parse(CNACreate)
-
-    // 1. make path absolute
-    let destDir = path.resolve(args.path)
-
-    // 2. Make sure we have npm, fatal otherwise
-    if (!isNpmInstalled()) { // todo: better error message
-      this.error('npm is required.')
-    }
-
-    // 3. create destination if not there
-    if (!fs.existsSync(destDir)) {
-      this.log('Creating dir for app: ', destDir)
-      fs.mkdirSync(destDir)
-    }
-    // 4. fail if destination is not empty
-    if (fs.readdirSync(destDir).length > 0) {
-      this.error('Expected destination path to be empty: ' + destDir)
-    }
-
-    // 5. use templateMap
-    let template = templateMap.createTemplates[flags.template]
-    if (template != null) {
-      // 6. try to get the template src from this repos templates folder
-      let srcDir = path.resolve(__dirname, '../../templates/', template.path)
-      if (fs.existsSync(srcDir)) {
-        fs.copySync(srcDir, destDir)
-        if (flags.deps) {
-          installTemplate(destDir)
-        } else {
-          this.log(`Dependencies were not installed.  Be sure to run 'npm install' inside ${destDir}`)
-        }
-      }
-    } else {
-      // todo: could try a fetch from npm
-      this.error(`'${flags.template}' does not appear to be a valid template name`)
-    }
+    const { args } = this.parse(CNACreate)
+    return InitCommand.run([args.path, '-y'])
   }
 }
 
 CNACreate.description = `Create a new Cloud Native Application
-
-Valid template names are ${Object.keys(templateMap.createTemplates).join(', ')}
 `
 
 CNACreate.flags = {
-  ...CNABaseCommand.flags,
-  template: flags.string({
-    char: 't',
-    // description: 'Template starter filepath, git-url or published id/name.',
-    description: 'template name to use',
-    default: 'basic-action-view-app'
-  }),
-  deps: flags.boolean({
-    description: 'Install dependencies by running `npm install` in the target directory',
-    default: true,
-    allowNo: true
-  })
-  // todo:
-  // name: flags.string( {
-  //   char: 'n',
-  //   description: 'Specify a name for your app'
-  // }),
+  ...CNABaseCommand.flags
 }
 
 CNACreate.args = CNABaseCommand.args
