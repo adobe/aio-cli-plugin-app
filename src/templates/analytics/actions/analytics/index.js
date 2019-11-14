6f54d800ea1f5ceff009fcadbef40576dd49eb16
Jie Yu <jieyu@adobe.com>
2019-11-14 05:12:50 -0800

    upgrade the analytic template

--- src/templates/analytics/actions/analytics/index.js
+++ src/templates/analytics/actions/analytics/index.js
@@ -1,15 +1,43 @@
 /**
- * main action
+ * Main action
+ *
+ * You can invoke this function via:
+ *     aio rt:action:invoke <action_path> -p tenant '<tenant_id>' -p apiKey '<api_key>' -p token '<access_token>'
+ *
+ * To find your <action_path>, run this command:
+ *     aio rt:ls
+ *
+ * To show debug logging for this function, you can add the LOG_LEVEL parameter as well:
+ *     aio rt:action:invoke <action_path> -p tenant '<tenant_id>' -p apiKey '<api_key>' -p token '<access_token>' -p LOG_LEVEL '<log_level>'
+ * ... where LOG_LEVEL can be one of [ error, warn, info, verbose, debug, silly ]
+ *
+ * Then, you can view your app logs:
+ *     aio app:logs
  */
 
-const Analytics = require('@adobe/aio-lib-analytics')
+const { Core, Analytics } = require('@adobe/aio-sdk')
 
 async function main (params) {
-  // Analytics example
-  const analyticsClient = await Analytics.init(params.companyId, params.apiKey, params.token)
-  const collections = await analyticsClient.getCollections({ limit: 5, page: 0 })
-  console.log('collections=', collections)
-  return collections
+  // create a Logger
+  const myAppLogger = Core.Logger('MyApp', { level: params.LOG_LEVEL })
+  // 'info' is the default level if not set
+  myAppLogger.info('Calling the main action')
+
+  // log levels are cumulative: 'debug' will include 'info' as well (levels are in order of verbosity: error, warn, info, verbose, debug, silly)
+  myAppLogger.debug(`params: ${JSON.stringify(params, null, 2)}`)
+
+  try {
+    // initialize the sdk
+    const analyticsClient = await Analytics.init(params.companyId, params.apiKey, params.token)
+
+    // get profiles from Campaign Standard
+    const collections = await analyticsClient.getCollections({ limit: 5, page: 0 })
+    myAppLogger.debug(`profiles = ${JSON.stringify(collections, null, 2)}`)
+
+    return collections
+  } catch (error) {
+    myAppLogger.error(error)
+  }
 }
 
 exports.main = main
