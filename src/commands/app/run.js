e5445daeaf4918d5123da2eed3b765ef09b92137
Jesse MacFadyen <purplecabbage@gmail.com>
2020-01-24 00:57:17 -0800

    save the cert to config when we generate it, ask the dev to accept it right away, reuse it. aka ACNA-640

--- src/commands/app/run.js
+++ src/commands/app/run.js
@@ -14,9 +14,11 @@ const ora = require('ora')
 const chalk = require('chalk')
 const fs = require('fs-extra')
 const path = require('path')
+const https = require('https')
 const { cli } = require('cli-ux')
 
 const { flags } = require('@oclif/command')
+const coreConfig = require('@adobe/aio-lib-core-config')
 
 const BaseCommand = require('../../BaseCommand')
 const AppScripts = require('@adobe/aio-app-scripts')
@@ -35,12 +37,57 @@ class Run extends BaseCommand {
       fs.ensureDirSync(path.dirname(PRIVATE_KEY_PATH))
       // if they do not exists, attempt to create them
       if (!fs.existsSync(PRIVATE_KEY_PATH) && !fs.existsSync(PUB_CERT_PATH)) {
-        // todo: store them in global config when we generate them, so we don't need
-        // to repeatedly accept them
-        const CertCmd = this.config.findCommand('certificate:generate')
-        if (CertCmd) {
-          const Instance = CertCmd.load()
-          await Instance.run([`--keyout=${PRIVATE_KEY_PATH}`, `--out=${PUB_CERT_PATH}`, `-n=${this.appName}.cert`])
+        // 1. do they exist in global config?
+        const devConfig = coreConfig.get('aio-dev.dev-keys')
+        if (devConfig) {
+          // yes? write them to file
+          fs.writeFile(PRIVATE_KEY_PATH, devConfig.privateKey)
+          fs.writeFile(PUB_CERT_PATH, devConfig.publicCert)
+        } else {
+          // 2. generate them
+          const CertCmd = this.config.findCommand('certificate:generate')
+          if (CertCmd) {
+            console.log('generating certificate')
+            const Instance = CertCmd.load()
+            await Instance.run([`--keyout=${PRIVATE_KEY_PATH}`, `--out=${PUB_CERT_PATH}`, '-n=DeveloperSelfSigned.cert'])
+          } else {
+            // could not find the cert command, not sure what we should do here
+          }
+          // 3. store them globally
+          const privateKey = (await fs.readFile(PRIVATE_KEY_PATH)).toString()
+          const publicCert = (await fs.readFile(PUB_CERT_PATH)).toString()
+          console.log('storing certificate')
+          coreConfig.set('aio-dev.dev-keys.privateKey', privateKey)
+          coreConfig.set('aio-dev.dev-keys.publicCert', publicCert)
+
+          // 4. ask the developer to accept them
+          const options = {
+            key: privateKey,
+            cert: publicCert
+          }
+
+          let certAccepted = false
+          const startTime = Date.now()
+          const server = https.createServer(options, function (req, res) {
+            certAccepted = true
+            res.writeHead(200)
+            res.end('Congrats, you have accepted the certificate and can now use it for development on this machine.\n' +
+            'You can close this window.')
+          })
+          server.listen(9080)
+          this.log('A self signed development certificate has been generated, you will need to accept it in your browser in order to use it.')
+          cli.open('https://localhost:9080')
+          cli.action.start('Waiting for the certificate to be accepted.')
+          while (!certAccepted && Date.now() - startTime < 20000) {
+            await cli.wait()
+          }
+          if (certAccepted) {
+            cli.action.stop()
+            this.log('Great, you accepted the certificate!')
+          } else {
+            cli.action.stop('timed out')
+          }
+          server.close()
         }
       }
       // if they now exist ... use them in the options
@@ -49,6 +96,8 @@ class Run extends BaseCommand {
           cert: PUB_CERT_PATH, // Path to custom certificate
           key: PRIVATE_KEY_PATH // Path to custom key
         }
+      } else {
+        // fatality?
       }
     } catch (error) {
       this.error(error)
