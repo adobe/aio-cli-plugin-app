23a181fd61d8a50d9259b97e69143ed9b019887f
florind12 <74616552+florind12@users.noreply.github.com>
2021-10-11 13:21:21 +0300

    [ACNA-1369] skip asset-compute for deleting.

--- src/commands/app/delete/action.js
+++ src/commands/app/delete/action.js
@@ -88,12 +88,9 @@ class DeleteActionCommand extends BaseCommand {
       try {
         const pathToE2eTests = path.join(action.e2eTestsDir, action.actionName + '.e2e.test.js')
         const pathToUnitTests = path.join(action.unitTestsDir, action.actionName + '.test.js')
-        // should we also delete asset-compute folder if empty?
-        const pathToAssetCompute = path.join(action.unitTestsDir, 'asset-compute', action.actionName)
+
         fs.removeSync(pathToE2eTests)
         aioLogger.debug(`deleted '${pathToE2eTests}'`)
-        fs.removeSync(pathToAssetCompute)
-        aioLogger.debug(`deleted '${pathToAssetCompute}'`)
         fs.removeSync(pathToUnitTests)
         aioLogger.debug(`deleted '${pathToUnitTests}'`)
 
