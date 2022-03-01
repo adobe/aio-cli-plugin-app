35e5c07cd95af9c26dfda8b2a289822790de32f7
Shazron Abdullah <shazron@gmail.com>
2022-03-02 01:55:53 +0800

    add template info tests. completed code coverage 100%

--- test/commands/app/template/info.test.js
+++ test/commands/app/template/info.test.js
@@ -12,6 +12,17 @@ governing permissions and limitations under the License.
 
 const TheCommand = require('../../../../src/commands/app/template/info')
 const BaseCommand = require('../../../../src/BaseCommand')
+const { TEMPLATE_PACKAGE_JSON_KEY, getNpmLocalVersion, readPackageJson } = require('../../../../src/lib/npm-helper')
+const { stdout } = require('stdout-stderr')
+
+jest.mock('../../../../src/lib/npm-helper', () => {
+  const orig = jest.requireActual('../../../../src/lib/npm-helper')
+  return {
+    ...orig,
+    readPackageJson: jest.fn(),
+    getNpmLocalVersion: jest.fn()
+  }
+})
 
 test('exports', async () => {
   expect(typeof TheCommand).toEqual('function')
@@ -28,30 +39,167 @@ test('aliases', async () => {
 
 test('flags', async () => {
   expect(TheCommand.flags.json).toBeDefined()
+  expect(TheCommand.flags.json.type).toEqual('boolean')
+
   expect(TheCommand.flags.yml).toBeDefined()
+  expect(TheCommand.flags.yml.type).toEqual('boolean')
 })
 
 test('args', async () => {
   expect(TheCommand.args).toEqual([])
 })
 
-// describe('instance methods', () => {
-//   let command
+describe('instance methods', () => {
+  let command
 
-//   beforeEach(() => {
-//     command = new TheCommand([])
-//   })
+  beforeEach(() => {
+    readPackageJson.mockReset()
+    getNpmLocalVersion.mockReset()
 
-//   describe('run', () => {
-//     test('exists', async () => {
-//       expect(command.run).toBeInstanceOf(Function)
-//     })
+    command = new TheCommand([])
+  })
 
-//     test('returns help file for app:list command', () => {
-//       const spy = jest.spyOn(HHelp.prototype, 'showHelp').mockReturnValue(true)
-//       return command.run().then(() => {
-//         expect(spy).toHaveBeenCalledWith(['app:template', '--help'])
-//       })
-//     })
-//   })
-// })
+  test('indentString', () => {
+    const string = 'mystring'
+    const result = command.indentString(string)
+    expect(result).toEqual('  ' + string)
+  })
+
+  test('printTemplate', () => {
+    command.printTemplate({ name: 'name', spec: '^1.0.0', version: '1.0.1' })
+    expect(stdout.output).toEqual('name@^1.0.0 (1.0.1)\n')
+  })
+
+  test('exists', async () => {
+    expect(command.run).toBeInstanceOf(Function)
+  })
+
+  describe('run', () => {
+    test('packages are installed', () => {
+      readPackageJson.mockResolvedValue({
+        dependencies: {
+          foo: '^1.0.0',
+          bar: '^2.0.0'
+        },
+        [TEMPLATE_PACKAGE_JSON_KEY]: [
+          'foo',
+          'bar'
+        ]
+      })
+
+      getNpmLocalVersion
+        .mockResolvedValueOnce('1.0.1')
+        .mockResolvedValueOnce('2.0.1')
+
+      command.argv = []
+      return command.run()
+        .then(() => {
+          expect(stdout.output).toMatch('foo@^1.0.0 (1.0.1)')
+          expect(stdout.output).toMatch('bar@^2.0.0 (2.0.1)')
+        })
+    })
+
+    test('no packages are installed', () => {
+      readPackageJson.mockResolvedValue({
+        dependencies: {
+          foo: '^1.0.0',
+          bar: '^2.0.0'
+        },
+        [TEMPLATE_PACKAGE_JSON_KEY]: [
+          'foo',
+          'bar'
+        ]
+      })
+
+      getNpmLocalVersion.mockRejectedValue(new Error('not found'))
+
+      command.argv = []
+      return command.run()
+        .then(() => {
+          expect(stdout.output).toMatch('foo@^1.0.0 (unknown)')
+          expect(stdout.output).toMatch('bar@^2.0.0 (unknown)')
+        })
+    })
+
+    test('no dependencies', () => {
+      readPackageJson.mockResolvedValue({
+        dependencies: {},
+        [TEMPLATE_PACKAGE_JSON_KEY]: [
+          'foo',
+          'bar'
+        ]
+      })
+
+      getNpmLocalVersion.mockRejectedValue(new Error('not found'))
+
+      command.argv = []
+      return command.run()
+        .then(() => {
+          expect(stdout.output).toMatch('foo@unknown (unknown)')
+          expect(stdout.output).toMatch('bar@unknown (unknown)')
+        })
+    })
+
+    test('no templates installed', () => {
+      readPackageJson.mockResolvedValue({
+        dependencies: {
+          foo: '^1.0.0',
+          bar: '^2.0.0'
+        }
+      })
+
+      command.argv = []
+      return command.run()
+        .then(() => {
+          expect(stdout.output).toMatch('no app templates are installed')
+        })
+    })
+
+    test('--yml', () => {
+      readPackageJson.mockResolvedValue({
+        dependencies: {
+          foo: '^1.0.0',
+          bar: '^2.0.0'
+        },
+        [TEMPLATE_PACKAGE_JSON_KEY]: [
+          'foo',
+          'bar'
+        ]
+      })
+
+      getNpmLocalVersion
+        .mockResolvedValueOnce('1.0.1')
+        .mockResolvedValueOnce('2.0.1')
+
+      command.argv = ['--yml']
+      return command.run()
+        .then(() => {
+          expect(stdout.output).toMatchFixture('templates/info.yml')
+        })
+    })
+
+    test('--json', () => {
+      readPackageJson.mockResolvedValue({
+        dependencies: {
+          foo: '^1.0.0',
+          bar: '^2.0.0'
+        },
+        [TEMPLATE_PACKAGE_JSON_KEY]: [
+          'foo',
+          'bar'
+        ]
+      })
+
+      getNpmLocalVersion
+        .mockResolvedValueOnce('1.0.1')
+        .mockResolvedValueOnce('2.0.1')
+
+      command.argv = ['--json']
+      return command.run()
+        .then(() => {
+          const json = global.fixtureJson('templates/info.json')
+          expect(stdout.output).toMatch(JSON.stringify(json, null, 2))
+        })
+    })
+  })
+})
