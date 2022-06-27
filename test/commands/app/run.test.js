a0e3d7500cdfa58d492adcacf8a18e5df92eaf75
Amulya Kashyap <amulyakashyap09@gmail.com>
2022-06-27 18:52:35 +0530

    fix: Variable should either be isLocal or isRemote throughout instead of negating the received value and passing it to another function.

--- test/commands/app/run.test.js
+++ test/commands/app/run.test.js
@@ -237,7 +237,7 @@ describe('run', () => {
     mockFSExists([PRIVATE_KEY_PATH, PUB_CERT_PATH])
     mockRunDev.mockImplementation((config, dataDir, options, logFunc) => {
       logFunc('boo')
-      expect(options.isLocal).toBe(false)
+      expect(options.isLocal).toBe(undefined)
     })
     command.argv = ['--verbose']
     command.getAppExtConfigs.mockReturnValueOnce(createAppConfig(command.appConfig))
@@ -251,7 +251,7 @@ describe('run', () => {
     mockFSExists([PRIVATE_KEY_PATH, PUB_CERT_PATH])
     mockRunDev.mockImplementation((config, dataDir, options, logFunc) => {
       logFunc('boo')
-      expect(options.isLocal).toBe(false)
+      expect(options.isLocal).toBe(undefined)
     })
     command.argv = []
     command.getAppExtConfigs.mockReturnValueOnce(createAppConfig(command.appConfig))
@@ -284,7 +284,7 @@ describe('run', () => {
       parcel: expect.objectContaining({
         logLevel: 'warn'
       }),
-      isLocal: false
+      isLocal: undefined
     }), expect.any(Function))
   })
 
@@ -313,7 +313,7 @@ describe('run', () => {
       parcel: expect.objectContaining({
         logLevel: 'verbose'
       }),
-      isLocal: false
+      isLocal: undefined
     }), expect.any(Function))
   })
 
