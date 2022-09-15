5f85a0a2af00c7d399194f4b3293019ab4f21a5e
Shazron Abdullah <shazron@gmail.com>
2022-09-16 01:42:32 +0800

    fix: app add extension to use Template Registry

--- src/commands/app/add/extension.js
+++ src/commands/app/add/extension.js
@@ -10,117 +10,45 @@ governing permissions and limitations under the License.
 */
 
 const AddCommand = require('../../../AddCommand')
-const yeoman = require('yeoman-environment')
 const aioLogger = require('@adobe/aio-lib-core-logging')('@adobe/aio-cli-plugin-app:add:action', { provider: 'debug' })
 const { Flags } = require('@oclif/core')
-const ora = require('ora')
-
-const { atLeastOne } = require('../../../lib/app-helper')
-const aioConfigLoader = require('@adobe/aio-lib-core-config')
-const { implPromptChoices } = require('../../../lib/defaults')
-const chalk = require('chalk')
+const TemplateRegistryAPI = require('@adobe/aio-lib-templates')
+const { selectTemplates, installTemplates } = require('../../../lib/templates-helper')
 
 class AddExtensionCommand extends AddCommand {
   async run () {
     const { flags } = await this.parse(AddExtensionCommand)
 
     aioLogger.debug(`add extensions with flags: ${JSON.stringify(flags)}`)
-    const spinner = ora()
+    // const spinner = ora()
 
     if (flags.yes && !flags.extension) {
       this.error('--extension= must also be provided when using --yes')
     }
 
-    const fullConfig = this.getFullConfig({ allowNoImpl: true })
-    const implementationsToAdd = await this.selectImplementations(flags, fullConfig)
+    // const fullConfig = this.getFullConfig({ allowNoImpl: true })
+    // TODO: fullConfig.implements is an array of extension ids already in the project (for filtering)
 
-    await this.runCodeGenerators(flags, implementationsToAdd)
-
-    await this.runInstallPackages(flags, spinner)
-
-    // warn about services to add
-    const workspaceServices =
-      aioConfigLoader.get('services') || // legacy
-      aioConfigLoader.get('project.workspace.details.services') ||
-      []
-
-    const supportedServiceCodesInWorkspace = new Set(workspaceServices.map(s => s.code))
-    implementationsToAdd.forEach(i => {
-      const missingServices = i.requiredServices.filter(s => !supportedServiceCodesInWorkspace.has(s))
-      if (missingServices.length > 0) {
-        this.warn(`Please add missing services '${missingServices}' required by '${i.name}'`)
-      }
-    })
-  }
-
-  async selectImplementations (flags, config) {
-    const alreadyImplemented = config.implements
-    const availableChoices = implPromptChoices
-    const availableImplementations = availableChoices.map(i => i.value.name)
-
-    const possibleChoices = availableChoices.filter(i => !alreadyImplemented.includes(i.value.name))
-    if (possibleChoices.length <= 0) {
-      throw new Error('All available extensions are already implemented in this project.')
+    const searchCriteria = {
+      [TemplateRegistryAPI.SEARCH_CRITERIA_EXTENSIONS]: TemplateRegistryAPI.SEARCH_CRITERIA_FILTER_ANY
+    }
+    const orderByCriteria = {
+      [TemplateRegistryAPI.ORDER_BY_CRITERIA_PUBLISH_DATE]: TemplateRegistryAPI.ORDER_BY_CRITERIA_SORT_DESC
     }
 
-    if (flags.extension) {
-      // no prompt
-      const alreadyThere = flags.extension.filter(i => alreadyImplemented.includes(i))
-      if (alreadyThere.length > 0) {
-        throw new Error(`'${alreadyThere}' is/are already implemented by this project`)
-      }
-      const invalid = flags.extension.filter(i => !availableImplementations.includes(i))
-      if (invalid.length > 0) {
-        throw new Error(`Invalid extension(s) '${invalid}', available implementations are '${availableImplementations}'`)
-      }
-
-      return flags.extension.map(i => possibleChoices.find(c => c.value.name === i).value)
-    }
-
-    // prompt
-    const answers = await this.prompt([{
-      type: 'checkbox',
-      name: 'res',
-      message: 'Which new implementation(s) do you wish to add to the project ?',
-      choices: possibleChoices,
-      validate: atLeastOne
-    }])
-
-    return answers.res
-  }
-
-  async runCodeGenerators (flags, implementations) {
-    const env = yeoman.createEnv()
-    // by default yeoman runs the install, we control installation from the app plugin
-    env.options = { skipInstall: true }
-    for (let i = 0; i < implementations.length; ++i) {
-      const implementation = implementations[i]
-      const gen = env.instantiate(implementation.generator,
-        {
-          options: {
-            'skip-prompt': flags.yes,
-            // no yeoman overwrite prompts
-            force: true
-          }
-        })
-      this.log(chalk.blue(chalk.bold(`Running generator for ${implementation.name}`)))
-      await env.runGenerator(gen)
-    }
+    const templates = await selectTemplates(searchCriteria, orderByCriteria)
+    const installer = (args) => this.config.runCommand('templates:install', args)
+    await installTemplates(flags.yes, false, templates, installer)
   }
 }
 
-AddExtensionCommand.description = `Add new extensions or a standalone application to the project
+AddExtensionCommand.description = `Add new extensions to the project
 `
 AddExtensionCommand.flags = {
   yes: Flags.boolean({
     description: 'Skip questions, and use all default values',
     default: false,
     char: 'y'
-  }),
-  extension: Flags.string({
-    description: 'Specify extensions to add, skips selection prompt',
-    char: 'e',
-    multiple: true
   }),
   ...AddCommand.flags
 }
