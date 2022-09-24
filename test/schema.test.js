2225901332e3a9eff9247b24410e621129faab89
Amulya Kashyap <amulyakashyap09@gmail.com>
2022-09-24 20:32:06 +0530

    fix: ACNA-1787 - app init error - integration_type oauthsinglepageapp (#577)
    Co-authored-by: Shazron Abdullah <36107+shazron@users.noreply.github.com>

--- test/schema.test.js
+++ test/schema.test.js
@@ -15,6 +15,8 @@ test('validate failure', () => {
   const valid = validate(fixtureJson('invalid.config.json'))
   // the 4 errors are the missing name properties, techacct migration to two new properties
   // the rest 3 are missing client_id and failing keyword `then`
-  expect(validate.errors.length).toEqual(7)
+  // 2 for failing if & else condition
+  // 1 for failing client_id required criteria
+  expect(validate.errors.length).toEqual(10)
   expect(valid).toBeFalsy()
 })
