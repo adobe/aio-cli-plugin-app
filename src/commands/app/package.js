06237c2f9658a9230596827ecd0e4fc5523da612
Shazron Abdullah <shaz@adobe.com>
2022-12-06 15:22:46 +0800

    add packaging stubs

--- src/commands/app/package.js
+++ src/commands/app/package.js
@@ -10,21 +10,45 @@ governing permissions and limitations under the License.
 */
 
 const BaseCommand = require('../../BaseCommand')
-const InitCommand = require('./init')
 const { Flags } = require('@oclif/core')
+const path = require('node:path')
+const { resolve } = require('node:path')
+const aioLogger = require('@adobe/aio-lib-core-logging')('@adobe/aio-cli-plugin-app:package', { provider: 'debug' })
 
 class Package extends BaseCommand {
   async run () {
     const { args, flags } = await this.parse(Package)
 
-    if (flags.import) {
-      return InitCommand.run([args.path, '-y', '--import', flags.import])
+    aioLogger.debug(`flags: ${JSON.stringify(flags, null, 2)}`)
+    aioLogger.debug(`args: ${JSON.stringify(args, null, 2)}`)
+
+    if (flags.output) {
+      // resolve to absolute path before any chdir
+      flags.output = path.resolve(flags.output)
     }
-    return InitCommand.run([args.path, '-y'])
+
+    // change the cwd if necessary
+    if (args.path !== '.') {
+      const resolvedPath = path.resolve(args.path)
+      process.chdir(resolvedPath)
+      aioLogger.debug(`changed current working directory to: ${resolvedPath}`)
+    }
+
+    // build phase
+    if (flags.build) {
+      await this.config.runCommand('app:build', [])
+    } else {
+      aioLogger.debug('skipping build')
+    }
+
+    this.log('TODO: create DD Metadata json based on configuration definition in app.config.yaml')
+    this.log('TODO: create install.yaml based on package.json, .aio, app.config.yaml, etc')
   }
 }
 
 Package.description = `Package a new Adobe I/O App for distribution
+
+This will always force a rebuild unless --no-force-build is set.
 `
 
 Package.flags = {
@@ -33,6 +57,17 @@ Package.flags = {
     description: 'The packaged app output file path',
     char: 'o',
     default: 'app.zip'
+  }),
+  build: Flags.boolean({
+    description: '[default: true] Run the build phase before packaging',
+    default: true,
+    allowNo: true
+  }),
+  'force-build': Flags.boolean({
+    description: '[default: true] Force a build even if one already exists',
+    exclusive: ['no-build'], // no-build
+    default: true,
+    allowNo: true
   })
 }
 
