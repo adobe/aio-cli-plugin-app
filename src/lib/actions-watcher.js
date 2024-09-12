b1619c36e1ad03f8a8555b77e695c1b489e8b993
Jesse MacFadyen <purplecabbage@gmail.com>
2024-09-11 17:22:18 -0700

    handle cases where manifest specifies action dir, and it changes during run

--- src/lib/actions-watcher.js
+++ src/lib/actions-watcher.js
@@ -38,7 +38,7 @@ const deployActions = require('./deploy-actions')
 module.exports = async (watcherOptions) => {
   const { config, log } = watcherOptions
 
-  log(`watching action files at ${config.actions.src}...`)
+  log(`watching action files at ${config.actions.src} ...`)
   const watcher = chokidar.watch(config.actions.src)
 
   watcher.on('change', createChangeHandler({ ...watcherOptions, watcher }))
@@ -91,6 +91,8 @@ function createChangeHandler (watcherOptions) {
     try {
       aioLogger.debug(`${filePath} has changed. Redeploying actions.`)
       const filterActions = getActionNameFromPath(filePath, watcherOptions)
+      // this is happening, but its not showing up because verbose is usually off
+      // this might be more important and worthy of signalling to the user
       if (!filterActions.length) {
         log('  -> A non-action file was changed, restart is required to deploy...')
       } else {
@@ -119,6 +121,13 @@ function createChangeHandler (watcherOptions) {
  * @returns {Array<string>}  All of the actions which match the modified path
  */
 function getActionNameFromPath (filePath, watcherOptions) {
+  // note: this check only happens during aio app run
+  // before the watcher is started, all actions are built and deployed and hashes updated
+  // this code is missing 2 cases:
+  // 1. if the action is a folder with a package.json and the changed file is in the folder
+  // 2. if the changed file is in the folder with the action, but not the action file itself
+  // we need to be careful with these cases, because we could cause a recursive loop
+  // for now we continue to error on the cautious side, and output a message suggesting a restart
   const actionNames = []
   const unixFilePath = upath.toUnix(filePath)
   const { config } = watcherOptions
@@ -126,7 +135,10 @@ function getActionNameFromPath (filePath, watcherOptions) {
     if (pkg.actions) {
       Object.entries(pkg.actions).forEach(([actionName, action]) => {
         const unixActionFunction = upath.toUnix(action.function)
-        if (unixActionFunction.includes(unixFilePath)) {
+        // since action could be a folder, and changed file could be in the folder
+        // we need to compare both ways
+        // there are 2 additional cases listed above
+        if (unixActionFunction.includes(unixFilePath) || unixFilePath.includes(unixActionFunction)) {
           actionNames.push(actionName)
         }
       })
